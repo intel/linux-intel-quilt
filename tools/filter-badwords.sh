a112b96f59bbe86882e99e4071e23cfc519ae146
Gross, Mark <mark.gross@intel.com>
2018-10-23 15:32:49 -0700

    update the banned word list filter branch code with more words to check.
    Change-Id: Ia8843f0b4ede3ecec995f58d7eecdb5a7ec5e8a3

--- tools/filter-badwords.sh
+++ tools/filter-badwords.sh
@@ -10,6 +10,27 @@ manifest=$2
 for b in `cat $manifest`; do
 	git checkout $b
 	git filter-branch -f --msg-filter 'sed "s/\S*\(kojiclear\)\S*//g"' $rc..
+	
+	git filter-branch -f --msg-filter 'sed "s/\S*\(tm.intel.com\)\S*//g"' $rc..
+	git filter-branch -f --msg-filter 'sed "s/\S*\(sh.intel.com\)\S*//g"' $rc..
+	git filter-branch -f --msg-filter 'sed "s/\S*\(ra.intel.com\)\S*//g"' $rc..
+	git filter-branch -f --msg-filter 'sed "s/\S*\(png.intel.com\)\S*//g"' $rc..
+	git filter-branch -f --msg-filter 'sed "s/\S*\(or.intel.com\)\S*//g"' $rc..
+	git filter-branch -f --msg-filter 'sed "s/\S*\(lm.intel.com\)\S*//g"' $rc..
+	git filter-branch -f --msg-filter 'sed "s/\S*\(ld.intel.com\)\S*//g"' $rc..
+	git filter-branch -f --msg-filter 'sed "s/\S*\(ka.intel.com\)\S*//g"' $rc..
+	git filter-branch -f --msg-filter 'sed "s/\S*\(jf.intel.com\)\S*//g"' $rc..
+	git filter-branch -f --msg-filter 'sed "s/\S*\(ikor.intel.com\)\S*//g"' $rc..
+	git filter-branch -f --msg-filter 'sed "s/\S*\(iind.intel.com\)\S*//g"' $rc..
+	git filter-branch -f --msg-filter 'sed "s/\S*\(iam.intel.com\)\S*//g"' $rc..
+	git filter-branch -f --msg-filter 'sed "s/\S*\(hf.intel.com\)\S*//g"' $rc..
+	git filter-branch -f --msg-filter 'sed "s/\S*\(gv.intel.com\)\S*//g"' $rc..
+	git filter-branch -f --msg-filter 'sed "s/\S*\(fm.intel.com\)\S*//g"' $rc..
+	git filter-branch -f --msg-filter 'sed "s/\S*\(fi.intel.com\)\S*//g"' $rc..
+	git filter-branch -f --msg-filter 'sed "s/\S*\(ch.intel.com\)\S*//g"' $rc..
+	git filter-branch -f --msg-filter 'sed "s/\S*\(bj.intel.com\)\S*//g"' $rc..
+	git filter-branch -f --msg-filter 'sed "s/\S*\(an.intel.com\)\S*//g"' $rc..
+	
 	git filter-branch -f --msg-filter 'sed "s/\S*\(zpn.intel.com\)\S*//g"' $rc..
 	git filter-branch -f --msg-filter 'sed "s/\S*\(jf.intel.com\)\S*//g"' $rc..
 	git filter-branch -f --msg-filter 'sed "s/\S*\(sc.intel.com\)\S*//g"' $rc..
